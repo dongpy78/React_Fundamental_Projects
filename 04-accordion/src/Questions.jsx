2bb30e0863023beb6f7ef3ddd3ef0bfe356957a3
Van Dong <vandong78py@gmail.com>
2024-10-23 13:41:54 +0700

    Question - Alternative Setup

--- 04-accordion/src/Questions.jsx
+++ 04-accordion/src/Questions.jsx
@@ -1,11 +1,18 @@
 import SingleQuestion from "./SingleQuestion";
 
-function Questions({ questions }) {
+function Questions({ questions, activeId, toggleQuestion }) {
   return (
     <section className="container">
       <h1 className="questions">Question</h1>
       {questions.map((question) => {
-        return <SingleQuestion key={question.id} {...question} />;
+        return (
+          <SingleQuestion
+            key={question.id}
+            {...question}
+            activeId={activeId}
+            toggleQuestion={toggleQuestion}
+          />
+        );
       })}
     </section>
   );
