5f48ed4ff546e8cb6c8d58416fc76b6b779d8fd0
Van Dong <vandong78py@gmail.com>
2024-10-26 17:28:49 +0700

    Comment Expland Logic Slider

--- 07-slider/src/Carousel.jsx
+++ 07-slider/src/Carousel.jsx
@@ -4,16 +4,23 @@ import { FaQuoteRight } from "react-icons/fa";
 import { FiChevronsLeft, FiChevronsRight } from "react-icons/fi";
 
 function Carousel() {
-  const [people, setPeople] = useState(list);
+  const [people, setPeople] = useState(list); // Trạng thái để lưu danh sách người. Bắt đầu với list.
+  // Trạng thái để lưu chỉ số của người hiện tại đang được hiển thị. Bắt đầu với 0, nghĩa là người đầu tiên.
   const [currentPerson, setCurrentPerson] = useState(0);
 
+  // Hàm này sẽ giảm chỉ số currentPerson để hiển thị người trước đó.
   const prevSlide = () => {
+    // Nếu oldPerson là 0, khi giảm xuống sẽ là -1. Để tránh âm,
+    //  ta cộng people.length rồi lấy phần dư với people.length để quay về cuối danh sách.
     setCurrentPerson((oldPerson) => {
       const result = (oldPerson - 1 + people.length) % people.length;
       return result;
     });
   };
+
+  // Hàm này sẽ tăng chỉ số currentPerson để hiển thị người tiếp theo.
   const nextSlide = () => {
+    // Nếu oldPerson là người cuối cùng, nó sẽ trở về 0 khi tăng chỉ số.
     setCurrentPerson((oldPerson) => {
       const result = (oldPerson + 1) % people.length;
       return result;
@@ -22,6 +29,7 @@ function Carousel() {
 
   return (
     <section className="slider-container">
+      {/* Mỗi person trong people được duyệt qua và tạo thành một article chứa thông tin của họ. */}
       {people.map((person, personIndex) => {
         const { id, image, name, title, quote } = person;
         return (
