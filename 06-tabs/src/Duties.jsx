e6e35c2563b535fc92b54e771e29330a971a3db4
Van Dong <vandong78py@gmail.com>
2024-10-26 15:09:42 +0700

    Tabs - Button Container

--- 06-tabs/src/Duties.jsx
+++ 06-tabs/src/Duties.jsx
@@ -1,6 +1,21 @@
+import { FaAngleDoubleRight } from "react-icons/fa";
+import { v4 as uuidv4 } from "uuid";
+
 function Duties({ duties }) {
   console.log(duties);
-  return <div></div>;
+  return (
+    <div>
+      {duties.map((duty, index) => {
+        const id = uuidv4();
+        return (
+          <div key={id} className="job-desc">
+            <FaAngleDoubleRight className="job-icon" />
+            <p>{duty}</p>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
 export default Duties;
