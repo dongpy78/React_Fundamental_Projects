ff8684ff62fdca4375374a2bb715ebfcbcb0004b
Van Dong <vandong78py@gmail.com>
2024-10-26 15:19:20 +0700

    Tabs - Current Item

---
+++ 06-tabs/src/BtnContainer.jsx
@@ -0,0 +1,15 @@
+function BtnContainer({ jobs }) {
+  return (
+    <div className="btn-container">
+      {jobs.map((item) => {
+        return (
+          <button className="job-btn" key={item.id}>
+            {item.company}
+          </button>
+        );
+      })}
+    </div>
+  );
+}
+
+export default BtnContainer;
