e6232beb71c0937a46ad3b6ccd1dd151e2698f73
Van Dong <vandong78py@gmail.com>
2024-10-29 16:02:48 +0700

    Color Generator - React Toastify

--- 09-color-generator/src/App.jsx
+++ 09-color-generator/src/App.jsx
@@ -2,15 +2,18 @@ import { useState } from "react";
 import ColorList from "./ColorList";
 import Form from "./Form";
 import Values from "values.js";
+import { ToastContainer, toast } from "react-toastify";
 
 const App = () => {
   // tạo ra mảng các màu sắc với các sắc thái sáng hơn và tối hơn của mã màu
   //  được tạo all(10) vào bảng điều khiển của trình duyệt
   const [colors, setColors] = useState(new Values("#f15025").all(10));
+
   return (
     <main>
       <Form />
       <ColorList colors={colors} />
+      <ToastContainer position="top-center" />
     </main>
   );
 };
