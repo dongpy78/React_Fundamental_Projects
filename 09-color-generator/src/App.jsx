b725caa705ba853d47d39e345fcdafc95ca98ff1
Van Dong <vandong78py@gmail.com>
2024-10-29 09:27:55 +0700

    Color Generator - Values.js Library

--- 09-color-generator/src/App.jsx
+++ 09-color-generator/src/App.jsx
@@ -1,7 +1,12 @@
+import { useState } from "react";
 import ColorList from "./ColorList";
 import Form from "./Form";
+import Values from "values.js";
 
 const App = () => {
+  // tạo ra mảng các màu sắc với các sắc thái sáng hơn và tối hơn của mã màu
+  //  được tạo all(10) vào bảng điều khiển của trình duyệt
+  const [colors, setColors] = useState(new Values("#f15025").all(10));
   return (
     <main>
       <Form />
