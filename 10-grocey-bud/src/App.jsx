e9cff74da0d02e560d723419e97860f60241b0ff
Van Dong <vandong78py@gmail.com>
2024-10-30 10:29:12 +0700

    Grocery Bud - Local Storage

--- 10-grocey-bud/src/App.jsx
+++ 10-grocey-bud/src/App.jsx
@@ -3,8 +3,30 @@ import Form from "./Form";
 import { nanoid } from "nanoid";
 import Items from "./Items";
 
+// lấy dữ liệu từ localStorage. Nếu có dữ liệu (danh sách),
+// nó sẽ chuyển đổi dữ liệu từ chuỗi JSON thành mảng. Nếu không có dữ liệu, nó sẽ trả về một mảng rỗng.
+const getLocalStorage = () => {
+  let list = localStorage.getItem("list");
+  if (list) {
+    list = JSON.parse(localStorage.getItem("list"));
+  } else {
+    list = [];
+  }
+  return list;
+};
+
+// Lưu trữ dữ liệu vào localStorage.
+// Dữ liệu được chuyển đổi từ mảng thành chuỗi JSON trước khi lưu.
+const setLocalStorage = (items) => {
+  localStorage.setItem("list", JSON.stringify(items));
+};
+
+// chứa danh sách mặc định khi ứng dụng khởi động.
+// Nó sử dụng hàm getLocalStorage để lấy dữ liệu từ localStorage hoặc trả về mảng rỗng nếu không có dữ liệu.
+const defaultList = JSON.parse(localStorage.getItem("list") || "[]");
+
 const App = () => {
-  const [items, setItems] = useState([]);
+  const [items, setItems] = useState(defaultList);
 
   const addItem = (itemName) => {
     const newItem = {
@@ -12,12 +34,15 @@ const App = () => {
       completed: false,
       id: nanoid(),
     };
-    setItems([...items, newItem]);
+    const newItems = [...items, newItem];
+    setItems(newItems);
+    setLocalStorage(newItems);
   };
 
   const removeItem = (itemId) => {
     const newItems = items.filter((item) => item.id !== itemId);
     setItems(newItems);
+    setLocalStorage(newItems);
   };
 
   return (
