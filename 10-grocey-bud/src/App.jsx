06d8481c1d329ba1aa4265ec30a55f42045f0463
Van Dong <vandong78py@gmail.com>
2024-10-30 10:57:45 +0700

    Grocery Bud - React Toastify

--- 10-grocey-bud/src/App.jsx
+++ 10-grocey-bud/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import Form from "./Form";
 import { nanoid } from "nanoid";
 import Items from "./Items";
+import { ToastContainer, toast } from "react-toastify";
 
 // lấy dữ liệu từ localStorage. Nếu có dữ liệu (danh sách),
 // nó sẽ chuyển đổi dữ liệu từ chuỗi JSON thành mảng. Nếu không có dữ liệu, nó sẽ trả về một mảng rỗng.
@@ -37,12 +38,14 @@ const App = () => {
     const newItems = [...items, newItem];
     setItems(newItems);
     setLocalStorage(newItems);
+    toast.success("item added to the list");
   };
 
   const removeItem = (itemId) => {
     const newItems = items.filter((item) => item.id !== itemId);
     setItems(newItems);
     setLocalStorage(newItems);
+    toast.success("item deleted");
   };
   // huhu
   const editItem = (itemId) => {
@@ -59,6 +62,7 @@ const App = () => {
 
   return (
     <section className="section-center">
+      <ToastContainer position="top-center" />
       <Form addItem={addItem} />
       <Items items={items} removeItem={removeItem} editItem={editItem} />
     </section>
