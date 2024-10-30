99fad1a12a61790ce3eedbe1ba70c19741e49b2d
Van Dong <vandong78py@gmail.com>
2024-10-30 16:50:17 +0700

    Navbar - Initial Setup

---
+++ 11-navbar/src/Navbar.jsx
@@ -0,0 +1,39 @@
+import { FaBars } from "react-icons/fa";
+import { links, social } from "./data";
+import logo from "./logo.svg";
+import { useState } from "react";
+
+function Navbar() {
+  const [showLinks, setShowLinks] = useState(true);
+  return (
+    <nav>
+      <div className="nav-center">
+        <div className="nav-header">
+          <img src={logo} className="logo" alt="logo" />
+          <button
+            className="nav-toggle"
+            onClick={() => setShowLinks(!showLinks)}
+          >
+            <FaBars />
+          </button>
+        </div>
+        {showLinks && (
+          <div className="links-container">
+            <ul className="links">
+              {links.map((link) => {
+                const { id, url, text } = link;
+                return (
+                  <li key={id}>
+                    <a href={url}>{text}</a>
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+        )}
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
