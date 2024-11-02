ef1a3a63f8ccb64f8fb5cd229dd0fb69762829ca
Van Dong <vandong78py@gmail.com>
2024-11-02 14:42:17 +0700

    Strapi - Components

---
+++ 13-strapi-submenus/src/Navbar.jsx
@@ -0,0 +1,19 @@
+import { FaBars } from "react-icons/fa";
+import { useGlobalContext } from "./Context";
+
+function Navbar() {
+  const { openSidebar } = useGlobalContext();
+  return (
+    <nav>
+      <div className="nav-center">
+        <h3 className="logo">strapi</h3>
+        <button className="toggle-btn" onClick={openSidebar}>
+          <FaBars />
+        </button>
+        {/* nav links later */}
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
