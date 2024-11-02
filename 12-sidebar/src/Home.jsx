eb4cda4de274114caf1bd18c598ea2c6a9bcc720
Van Dong <vandong78py@gmail.com>
2024-11-02 10:22:33 +0700

    Sidebar - Home component

--- 12-sidebar/src/Home.jsx
+++ 12-sidebar/src/Home.jsx
@@ -1,9 +1,21 @@
 import { useGlobalContext } from "./context";
+import { FaBars } from "react-icons/fa";
 
 function Home() {
   const { openSidebar, openModal } = useGlobalContext();
   console.log(openSidebar, openModal);
-  return <div>huhu</div>;
+  return (
+    <main>
+      <div>
+        <button onClick={openSidebar} className="sidebar-toggle">
+          <FaBars />
+        </button>
+        <button onClick={openModal} className="btn">
+          show modal
+        </button>
+      </div>
+    </main>
+  );
 }
 
 export default Home;
