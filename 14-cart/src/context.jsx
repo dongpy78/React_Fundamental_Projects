66b55d9121beafa7fa5930f454e39261f158394d
Van Dong <vandong78py@gmail.com>
2024-11-02 17:26:03 +0700

    Cart - Setup Actions

--- 14-cart/src/context.jsx
+++ 14-cart/src/context.jsx
@@ -1,11 +1,27 @@
 import { useContext, useReducer, useEffect, createContext } from "react";
+import reducer from "./reducer";
+
+import {
+  CLEAR_CART,
+  REMOVE,
+  INCREASE,
+  DECREASE,
+  LOADING,
+  DISPLAY_ITEMS,
+} from "./actions";
 
 const AppContext = createContext();
 
+const initialState = {
+  loading: false,
+  cart: [],
+};
+
 export const AppProvider = ({ children }) => {
-  const greeting = "hello";
+  const [state, dispatch] = useReducer(reducer, initialState);
+
   return (
-    <AppContext.Provider value={{ greeting }}>{children}</AppContext.Provider>
+    <AppContext.Provider value={{ ...state }}>{children}</AppContext.Provider>
   );
 };
 
